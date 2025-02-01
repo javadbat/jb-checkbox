a6e4c50e5833b5a5fb4d3d26436504ff3ef46761
javadbat <javadbat@gmail.com>
2025-02-01 15:37:37 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,26 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-checkbox",
+    path: "./lib/jb-checkbox.ts",
+    outputPath: "./dist/jb-checkbox.js",
+    umdName: "JBCheckbox",
+    external: ["jb-validation", "jb-form"],
+    globals: {
+      "jb-validation": "JBValidation",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-checkbox-react",
+    path: "./react/lib/JBCheckbox.tsx",
+    outputPath: "./react/dist/JBCheckbox.js",
+    external: ["prop-types", "react", "jb-checkbox"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
