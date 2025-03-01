d961347b0bfa43ef77725f4cab7ea3c71e906db0
javadbat <javadbat@gmail.com>
2025-03-02 00:15:25 +0330

    update build config

--- build-config.ts
+++ build-config.ts
@@ -6,9 +6,11 @@ export const webComponentList: WebComponentBuildConfig[] = [
     path: "./lib/jb-checkbox.ts",
     outputPath: "./dist/jb-checkbox.js",
     umdName: "JBCheckbox",
-    external: ["jb-validation", "jb-form"],
+    external: ["jb-validation", "jb-form", "jb-core"],
     globals: {
       "jb-validation": "JBValidation",
+      "jb-form": "JBForm",
+      "jb-core": "JBCore",
     },
   },
 ];
@@ -17,11 +19,13 @@ export const reactComponentList: ReactComponentBuildConfig[] = [
     name: "jb-checkbox-react",
     path: "./react/lib/JBCheckbox.tsx",
     outputPath: "./react/dist/JBCheckbox.js",
-    external: ["prop-types", "react", "jb-checkbox"],
+    external: ["prop-types", "react", "jb-checkbox","jb-core"],
     globals: {
       react: "React",
       "jb-checkbox": "JBCheckbox",
       "prop-types": "PropTypes",
+      "jb-core": "JBCore",
+      "jb-core/react": "JBCoreReact",
     },
     umdName:"JBCheckboxReact",
     dir:"./react"
